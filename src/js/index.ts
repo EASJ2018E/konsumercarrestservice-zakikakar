b7aaa5e5c217ca16f7c23e91aff52072fbc81614
zakikakar <36473212+zakikakar@users.noreply.github.com>
2018-10-24 11:09:18 +0200

    tilføjet eventlistner og functioner så knappen returnerer cars

--- src/js/index.ts
+++ src/js/index.ts
@@ -4,9 +4,31 @@ import axios, {
 
    // http://rest-pele-easj-dk.azurewebsites.net/api/Cars
 
-   interface Icar 
+   interface ICar 
    {
     model:string;
     vendor:string;
     price:number;
+   }
+
+   let buttonElement:HTMLButtonElement = <HTMLButtonElement> document.getElementById("getAllButton");
+   buttonElement.addEventListener('click', showAllCars);
+
+   function showAllCars():void {
+
+    let uri:string = "http://rest-pele-easj-dk.azurewebsites.net/api/Cars"; //Viser stien til vores api
+
+    axios.get<ICar[]>(uri) //Vi får et array af Icar
+    .then(function (response:AxiosResponse<ICar[]>):void
+    {
+        let result:string = "<ol>";
+        response.data.forEach((car:ICar) => {
+            result += "<li>"+ car.model + car.vendor + car.price.toString() +"</li>"
+        } );
+
+        result += "</ol>";
+
+    }
+    ) 
+    .catch()
    }
